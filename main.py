a3a915d2599e73be349ffc9d86ee3fcd9cc897c5
kangwlgns <kangji0615@gmail.com>
2024-06-04 20:10:41 +0900

    conf: add .gitignore

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-def fizz_buzz(i):
-    if i%15==0:
-        return true
-    else:
-        return false
-
-def fizz(i):
-    if i%3==0:
-        return true
-    else:
-        return false
-
-def buzz(i):
-    if i%5==0:
-        return true
-    else:
-        return false
+def fizz_buzz():
+    print("fizzbuzz")
 
 if __name__ == '__main__':
-    n = int(input())
-    for i in 1, n+1:
-        if fizz_buzz(i):
-            print("FizzBuzz", end=' ')
-        elif fizz(i):
-            print("Fizz")
-        elif buzz(i):
-            print("Buzz", end=' ')
-        else:
-            print(i, end=' ')
     fizz_buzz()
